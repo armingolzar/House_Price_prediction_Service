82f19ace015e0875c749d7b32e5d5a5061783823
Armin Golzar <armingolzar78@gmail.com>
2025-10-11 20:59:23 +0330

    CI revised and complete first version

--- test_prediction.py
+++ test_prediction.py
@@ -1,9 +1,57 @@
+import numpy as np
 import pytest 
 from src.api.app import app
 from fastapi.testclient import TestClient
+from unittest.mock import patch
 
 
-def test_predict_endpoint():
+class DummyScaler:
+
+    def transform(self, X):
+
+        return np.ones((len(X), len(X[0])))
+    
+class DummyEncoder1:
+    
+    def transform(self, X):
+        
+        return np.ones((len(X), len(X[0])))
+    
+class DummyEncoder2:
+
+    def transform(self, X):
+
+        return np.ones(len(X))
+    
+
+class DummyModel:
+
+    def predict(self, X):
+
+        return [9000000]
+
+
+
+@patch('src.api.app.load_model')
+@patch('src.api.app.load')
+def test_predict_endpoint(mock_load, mock_load_model):
+
+    def load_side_effect(path):
+
+        if 'standard_scaler_numerical' in path:
+            return DummyScaler()
+        
+        if 'ordinal_encoder_category' in path:
+            return DummyEncoder1
+        
+        if 'ordinal_encoder_ordinal' in path:
+            return DummyEncoder2
+        
+        else:
+            return None
+        
+    mock_load.side_effect = load_side_effect
+    mock_load_model.return_value = DummyModel()
 
     with TestClient(app) as client:
 
